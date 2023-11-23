2335df41c6258a829f02cf5e762bd31b54c65f99
Wład Artemenko <artemenko.wlad@gmail.com>
2023-11-23 09:25:00 +0100

    completed link buttons in Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -46,22 +46,29 @@ export default function Intro() {
                 I enjoy building modern looking <span className='italic'>sites & apps</span>. My main focus is <span className='underline'>React (Next.js)</span>
             </motion.p>
             <motion.div
-                className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+                className='flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium'
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{
                     delay: 0.1
                 }}
             >
-                <Link href="#contact" className='flex items-center bg-gray-900 text-white px-7 py-3 rounded-full gap-2'>Contact me here <BsArrowRight />
+                <Link href="#contact" className='group cursor-pointer flex items-center bg-gray-900 text-white px-7 py-3 rounded-full gap-2 outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all'>Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition-all" />
                 </Link>
-                <a className='flex items-center bg-white px-7 py-3 rounded-full gap-2'>Download CV <HiDownload /> </a>
-                <a className='flex items-center bg-white text-gray-700 p-4 rounded-full gap-2'>
-                    <BsLinkedin />
+
+
+                <a href='/CV.pdf' download className='group cursor-pointer border border-black/10 flex items-center bg-white px-7 py-3 rounded-full gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all'>Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition-all" /> </a>
+
+                {/* LinkedIn link */}
+                <a href='https://www.linkedin.com/in/kod3cz/' target='_blank' className='group cursor-pointer border border-black/10 flex items-center bg-white text-gray-700 p-4 rounded-full gap-2 outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all'>
+                    <BsLinkedin className="group-hover:scale-110 transition-all" />
                 </a>
-                <a className='flex items-center bg-white text-gray-700 p-4 rounded-full gap-2 text-[1.35rem]'>
-                    <FaGithubSquare />
+
+                {/* Github linnk */}
+                <a href='https://github.com/w14d3k' target='_blank' className='group cursor-pointer border border-black/10 flex items-center bg-white text-gray-700 p-4 rounded-full gap-2 text-[1.35rem] outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all'>
+                    <FaGithubSquare className="group-hover:scale-110 transition-all" />
                 </a>
+
             </motion.div>
         </section>
     )
